21536828f5b395f25aadb2a1b095699c38f8c978
SuryaMisan <63390445+surya111226@users.noreply.github.com>
2020-05-12 20:33:16 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$headers1 = "X-User-Id: Facebook_160792042140374";
+$headers2 = "X-Raw-User-Id: 4333f18a-7787-451a-be00-33ef50f4f4d2";
+$headers3 = "X-User-Name: Akin+Cod";
+$headers4 = "X-Invite-Code: FjepiAVWA";
+$headers5 = "X-User-Token: AbyLqvkeIrgUyYns";
+$headers6 = "X-Own-Mac: 02%3A00%3A00%3A00%3A00%3A00";
+$headers7 = "X-Android-Id: 6a1914b0afdf6c32";
+$headers8 = "X-Device-Type: OPPO++CPH1801";
+
+
+?>
